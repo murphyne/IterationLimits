36f13a0d8db7b69bd320984aa9c012c091a9f6ce
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2022-12-02 20:34:42 +0600

    Add `Func<bool>` tests with wrapped `Enumerator.MoveNext`

---
+++ IterationLimitsTests/FuncEnumeratorCountTests.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using IterationLimits;
+using NUnit.Framework;
+
+namespace IterationLimitsTests
+{
+    [TestFixture]
+    public class FuncEnumeratorCountTests
+    {
+        private int _counter = 0;
+        private const int Limited = 1_000;
+        private const int Unlimited = 1_000_000;
+
+        private IEnumerator<int> _enumerator;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _counter = 0;
+            _enumerator = GetNumbers().GetEnumerator();
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            _enumerator = null;
+        }
+
+        [Test]
+        public void TestSetup()
+        {
+            Assert.AreEqual(0, _counter);
+        }
+
+        [Test]
+        public void TestUnlimited()
+        {
+            Func<bool> conditionUnlimited = () => _enumerator.MoveNext();
+
+            while (conditionUnlimited.Invoke())
+            {
+                _counter += 1;
+            }
+
+            Assert.AreEqual(Unlimited, _counter);
+        }
+
+        [Test]
+        public void TestLimited()
+        {
+            Func<bool> conditionUnlimited = () => _enumerator.MoveNext();
+            Func<bool> conditionLimited = Limits.LimitCount(Limited, conditionUnlimited);
+
+            while (conditionLimited.Invoke())
+            {
+                _counter += 1;
+            }
+
+            Assert.AreEqual(Limited, _counter);
+        }
+
+        private IEnumerable<int> GetNumbers()
+        {
+            var i = 0;
+            yield return i;
+
+            while (_counter < Unlimited)
+            {
+                i += 1;
+                yield return i;
+            }
+        }
+    }
+}
---
+++ IterationLimitsTests/FuncEnumeratorTimeTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using IterationLimits;
+using NUnit.Framework;
+
+namespace IterationLimitsTests
+{
+    [TestFixture]
+    public class FuncEnumeratorTimeTests
+    {
+        private TimeSpan Elapsed => _now - _start;
+        private DateTime _start = DateTime.Now;
+        private DateTime _now = DateTime.Now;
+
+        private static readonly TimeSpan Error = TimeSpan.FromMilliseconds(10);
+        private static readonly TimeSpan Limited = TimeSpan.FromMilliseconds(200);
+        private static readonly TimeSpan Unlimited = TimeSpan.FromMilliseconds(600);
+
+        private IEnumerator<int> _enumerator;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _start = DateTime.Now;
+            _now = DateTime.Now;
+            _enumerator = GetNumbers().GetEnumerator();
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            _enumerator = null;
+        }
+
+        [Test]
+        public void TestSetup()
+        {
+            Assert.That(Elapsed, Is.EqualTo(TimeSpan.Zero).Within(Error));
+        }
+
+        [Test]
+        public void TestUnlimited()
+        {
+            Func<bool> conditionUnlimited = () => _enumerator.MoveNext();
+
+            while (conditionUnlimited.Invoke())
+            {
+                _now = DateTime.Now;
+            }
+
+            Assert.That(Elapsed, Is.EqualTo(Unlimited).Within(Error));
+        }
+
+        [Test]
+        public void TestLimited()
+        {
+            Func<bool> conditionUnlimited = () => _enumerator.MoveNext();
+            Func<bool> conditionLimited = Limits.LimitTime(Limited, conditionUnlimited);
+
+            while (conditionLimited.Invoke())
+            {
+                _now = DateTime.Now;
+            }
+
+            Assert.That(Elapsed, Is.EqualTo(Limited).Within(Error));
+        }
+
+        private IEnumerable<int> GetNumbers()
+        {
+            var i = 0;
+            yield return i;
+
+            while (Elapsed < Unlimited)
+            {
+                i += 1;
+                yield return i;
+            }
+        }
+    }
+}
